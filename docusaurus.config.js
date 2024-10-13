4886a081dd8dbb1411138440a754f5d66dd54fc7
sirbuig <sirbuig@proton.me>
2024-10-13 21:20:24 +0300

    Fix broken link and update GitHub Pages settings
    - Corrected the broken link in the project
    - Updated GitHub Pages configuration for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://sirbuig.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/operating-systems/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -45,7 +45,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/sirbuig/operating-systems/tree/main/',
         },
         blog: false,
         theme: {
